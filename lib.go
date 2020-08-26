71568e01ee4a8d9c5849000b12a60f9029c06f4a
Nathan Marley <nathan.marley@gmail.com>
2020-08-26 18:29:51 -0300

    No idea, old changes

--- lib.go
+++ lib.go
@@ -1,11 +1,18 @@
 package something
 
 import (
+	"expvar"
 	"fmt"
 
 	// import badger
 	_ "github.com/dgraph-io/badger"
 )
+
+var NumReads *expvar.Int
+
+func init() {
+	NumReads = expvar.NewInt("badger_disk_reads_total")
+}
 
 // Hi says hi
 func Hi() {
